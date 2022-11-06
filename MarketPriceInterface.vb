b54d1843bbe95e189ca74c142135dfc684aff3bd
Zifrian <ZifrianEVE@gmail.com>
2022-11-05 20:20:56 -0400

    Update MarketPriceInterface.vb

--- MarketPriceInterface.vb
+++ MarketPriceInterface.vb
@@ -371,6 +371,10 @@ Public Class MarketPriceInterface
         Dim rsCache As SQLiteDataReader
         Dim Cachedate As Date
 
+        If CacheType = MarketPriceCacheType.History Then
+            Return False
+        End If
+
         ' First look up the cache date to see if it's time to run the update
         If CacheType = MarketPriceCacheType.History Then
             TableName = "MARKET_HISTORY_UPDATE_CACHE"
