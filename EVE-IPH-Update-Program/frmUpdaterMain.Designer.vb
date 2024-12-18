91b2a70b6b639df907614cf35bc69dfc5133137b
Zifrian <ZifrianEVE@gmail.com>
2024-12-18 16:43:18 -0500

    Rebuild

--- EVE-IPH-Update-Program/frmUpdaterMain.Designer.vb
+++ EVE-IPH-Update-Program/frmUpdaterMain.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frmUpdaterMain
+Partial Class FrmUpdaterMain
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
